938f727a990fbfed98dc22e7253c55fc82aa5735
Javier Vazquez <jvrvzqzp@gmail.com>
2018-01-17 09:33:22 -0700

    Add files via upload

---
+++ Control.c
@@ -0,0 +1,122 @@
+/* 
+ * File:   goramit.c
+ * Author: Javier Vazquez
+ *
+ * Created on 19 de mayo de 2017, 02:04 AM
+ */
+#include <18F4550.h>
+
+#device adc=8
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+#fuses HS, NOWDT, XT
+#fuses NOPROTECT, BROWNOUT, PUT, NOLVP
+#use delay(clock=4MHz)
+
+void servo0(){
+    int i;
+for(i=0;i<70;i++){
+            output_high(PIN_B0);
+            delay_us(160);
+            output_low(PIN_B0);
+            delay_us(9840);
+        }
+}
+void servo90(){
+    int i;
+for(i=0;i<70;i++){
+            output_high(PIN_B0);
+            delay_us(405);
+            output_low(PIN_B0);
+            delay_us(9595);
+        }
+}
+void servo180(){
+    int i;
+for(i=0;i<70;i++){
+            output_high(PIN_B0);
+            delay_us(800);
+            output_low(PIN_B0);
+            delay_us(9200);
+        }
+}
+void servo45(){
+    int i;
+for(i=0;i<70;i++){
+            output_high(PIN_B0);
+            delay_us(273);
+            output_low(PIN_B0);
+            delay_us(9728);
+        }
+}
+void servo135(){
+    int i;
+for(i=0;i<70;i++){
+            output_high(PIN_B0);
+            delay_us(510);
+            output_low(PIN_B0);
+            delay_us(9490);
+        }
+}
+/* 
+ *
+ */
+int j=1;
+int i;
+void main() {
+    long count;
+    setup_timer_0(T0_EXT_H_TO_L|RTCC_DIV_1);
+    set_timer0(0);
+
+    output_low(PIN_B0);
+    delay_ms(500);
+    
+     output_low(PIN_B2);
+while(1){ 
+    set_timer0(0);
+    count =0;
+    servo90();
+    for (i = 0; i<50; i++){
+        output_high(PIN_B2);
+    count = get_timer0();
+    delay_ms(10);
+    }
+    output_low(PIN_B2);    
+       if (count > 2 || count == 0){
+            if (count > 10){
+                set_timer0(0);
+                count =0;
+            }
+            else{
+                servo90();
+                output_low(PIN_B2);
+                delay_ms(1000);
+            }
+        }
+
+    
+        
+        if (count % 2 == 0){
+            //output_high(PIN_B2);
+            servo135();
+            
+            delay_ms(2000);
+            set_timer0(0);
+                count =0;
+
+        }
+        else{
+            //output_high(PIN_B2);
+            servo45();  
+            delay_ms(2000);
+            set_timer0(0);
+                count =0;
+        }
+    
+    }
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ControlServo.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/ControlServo.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=controlservo.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/controlservo.x.tar
